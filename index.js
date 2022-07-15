c1ed24768ac89473f2b532631c4b82e96f5f4d46
Ivonete Tecla Gomes <lucasvicenti60@gmail.com>
2022-07-15 09:11:08 -0300

    inicial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function sobre_mim(){
+    document.querySelector("iframe").src = "sobre_mim.html"
+}
+function repositorios(){
+    document.querySelector("iframe").src = "repositorios.html"
+}
+function contatos(){
+    document.querySelector("iframe").src = "contato.html"
+}
